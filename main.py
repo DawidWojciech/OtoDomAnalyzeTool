0f305fa1199e4304efb6a6324fa2cbeb9ba33358
dawutek <d.wojciechowski@falconvsystems.com>
2021-01-15 12:05:32 +0100

    support for building plots added

--- main.py
+++ main.py
@@ -16,9 +16,9 @@ offer_parser = OfferParser('offers.json')
 
 if __name__ == '__main__':
 
-    # process.crawl(PropertyCrawler)
-    # process.start()
-    # offer_parser.save_as_json('parsed.json')
+    process.crawl(PropertyCrawler)
+    process.start()
+    offer_parser.save_as_json('parsed.json')
     map_generator = MapGenerator(data_file="parsed.json", map_file='data.html')
     map_generator.add_markers_to_map()
     map_generator.generate_map()
--- property_data_parser/map.py
+++ property_data_parser/map.py
@@ -17,11 +17,11 @@ class MapGenerator:
 
         for location, offers in self.locations.items():
             if len(offers) == 1:
-                self.generate_marker(offers[0]).add_to(self.map)
+                self.generate_marker_on_map(offers[0]).add_to(self.map)
             else:
                 marker_cluster = MarkerCluster()
                 for offer in offers:
-                    marker_cluster.add_child(self.generate_marker(offer))
+                    marker_cluster.add_child(self.generate_marker_on_map(offer))
                 self.map.add_child(marker_cluster)
 
     def generate_map(self):
@@ -35,15 +35,55 @@ class MapGenerator:
 
         return data
 
-    def generate_marker(self, offer):
+    def generate_marker_on_map(self, offer):
+
+        if offer['property_type'] == 'Dom':
+            marker = self.generate_marker_for_home(offer)
+        elif offer['property_type'] == 'Działka':
+            marker = self.generate_marker_for_building_plot(offer)
+
+        return marker
+
+    def generate_html_dependly_of_property_type(self, offer):
+
+        if offer['property_type'] == 'Dom':
+            html = self.generate_html_for_house(offer)
+        elif offer['property_type'] == 'Działka':
+            html = self.generate_html_for_building_plot(offer)
+
+        return html
+
+    @staticmethod
+    def generate_html_for_house(offer):
         html = f"<b>Typ nieruchomości:</b> {offer['property_type']}<br>" \
                f"<b>Cena:</b> {offer['price']}<br>" \
                f"<b>Ilość pokoi:</b> {offer['rooms']}<br>" \
                f"<b>Powierzchnia domu:</b> {offer['area_1']}<br>" \
                f"<a href='{offer['link']}' target='_blank'>Link do oferty</a>"
+        return html
+
+    @staticmethod
+    def generate_html_for_building_plot(offer):
+        html = f"<b>Typ nieruchomości:</b> {offer['property_type']}<br>" \
+               f"<b>Cena:</b> {offer['price']}<br>" \
+               f"<b>Powierzchnia działki:</b> {offer['area_1']}<br>" \
+               f"<a href='{offer['link']}' target='_blank'>Link do oferty</a>"
+        return html
+
+    def generate_marker_for_home(self, offer):
+        html = self.generate_html_dependly_of_property_type(offer)
+        icon = folium.map.Icon(color='blue', icon="glyphicon-home")
+        return self.marker_object(offer, html, icon)
+
+    def generate_marker_for_building_plot(self, offer):
+        html = self.generate_html_dependly_of_property_type(offer)
+        icon = folium.map.Icon(color='black', icon="glyphicon-tree-conifer")
+        return self.marker_object(offer, html, icon)
+
+    @staticmethod
+    def marker_object(offer, html, icon):
         iframe = folium.IFrame(html=html, width=300, height=110)
         popup = folium.Popup(iframe, max_width=400)
         marker = folium.Marker([float(offer["coordinates"]["lat"]), float(offer["coordinates"]["long"])],
-                               popup=popup)
-
+                               popup=popup, icon=icon)
         return marker
